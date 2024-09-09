f0cb064480ed7057ac510f602275cc29d7a4eb9d
Mouhamed Ndiaye <mouhamethndiaye123@gmail.com>
2024-09-09 21:30:59 +0000

    update file

--- tache5_2.js
+++ tache5_2.js
@@ -1,9 +1,18 @@
 // Écrire un script qui permet d'afficher la table de multiplication d'un nombre entier n.
+// a refaire avc dom
+
 
 let nombre = parseInt(prompt('Saisir un nombre'));
-    // document.write(typeof nombre)
-    console.log('la table de '+ nombre + ' est: ');
+
+const title = document.getElementById('subtitle');
+title.textContent = 'la table de '+ nombre +' est: ';
+
+const para = document.getElementsByTagName('p');
+
+for(let i = 1; i <= 10; i++) {
+    let resultat = nombre * i;
     
-for(let i = 1; i<=10; i++){
-    console.log(nombre +' x ' + i +' = ' + nombre*i);
+    if (para[i-1]) { 
+        para[i-1].textContent = nombre + ' x ' + i + ' = ' + resultat;
+    }
 }
